ae20c37662cc330c8027d80651e60d72e96d6a0a
Ivan Chernovalov <rulesprog@gmail.com>
2024-12-23 22:40:42 +0200

    almost ( :( ) done

--- exam/src/screens/history/History.tsx
+++ exam/src/screens/history/History.tsx
@@ -1,13 +1,24 @@
 import {useNavigate} from 'react-router-dom'
+import {useShallow} from 'zustand/react/shallow'
 import MaterialSymbol from 'components/MaterialSymbol/MaterialSymbol.tsx'
 import {paths} from '~/App.tsx'
+import {useAppStore} from '~/store.ts'
 import styles from './History.module.scss'
 
 function History() {
   const navigate=useNavigate()
+  const history = useAppStore( useShallow((state) =>state.history))
+    
   return <>History
     <button onClick={() => navigate(paths.translate)}><MaterialSymbol symbol="arrow_back" className={styles.compose}/>
     </button>
+    {history.map((item) => 
+      <div key={`${item.l1}${item.l2}${item.i}`}>
+        <div>{item.l1}-&gt;{item.l2}</div>
+        <div>{item.i}</div>
+        <div>{item.o}</div>
+      </div>
+    )}
   </>
 }
 
--- exam/src/screens/translate/Translate.tsx
+++ exam/src/screens/translate/Translate.tsx
@@ -3,10 +3,10 @@ import {KeyboardEventHandler, useEffect, useRef, useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
 import Select from 'react-select'
 import MaterialSymbol from 'components/MaterialSymbol/MaterialSymbol.tsx'
+import {useShallow} from 'zustand/react/shallow'
 import {paths} from '~/App.tsx'
 import {languages}  from '~/languages'
-// import {useMessengerStore} from '~/store.ts'
-import {time, useAppStore} from '~/store.ts'
+import {useAppStore} from '~/store.ts'
 import styles from './Translate.module.scss'
 
 function toOptions(object:Record<string, string>){
@@ -25,17 +25,15 @@ function Translate() {
     if (textareaRef.current === null) return
     const textarea = textareaRef.current
     if (textarea.value.trim().length === 0) return
-    // await api('messages/POST',{chat:chatId,files:fileInput.files, text: textarea.value},true,true)
-    const api = `https://api.mymemory.translated.net/get?q=${textarea.value}&langpair=${language1}|${language2}`
-    const r = await fetch(api).then(r => r.json())
-    setResult(r.responseData.translatedText)
-    console.log(r)
+    setInput(textarea.value)
   }
 
-  const [result, setResult] = useState('')
-  const [language1, setLanguage1] = useState('Autodetect')
-  const [language2, setLanguage2] = useState('en-GB')
-  const [language,setLanguage] = [useAppStore((state) => state.language),useAppStore((state) => state.set)]
+
+  const [language1,language2,output,setInput,setLanguage1,setLanguage2] =
+    useAppStore(useShallow((state) =>
+      [state.language1,state.language2,state.output,state.setInput,state.setLanguage1,state.setLanguage2])
+    )
+
   const textareaOnKeypress: KeyboardEventHandler<HTMLTextAreaElement> = (e) => {
     if (
       e.key !== 'Enter' ||
@@ -44,35 +42,29 @@ function Translate() {
     e.preventDefault()
     translate()
   }
-  // const increasePopulation = useMessengerStore((state) => state.increase)
-  // const [chats, setChats] = useState<>([])
   const navigate=useNavigate()
-  useEffect(() => {
-    (async ()=>{
-      // setChats(chatPropsAdapter((await api('chats/GET', {})).results))
 
-    })()
-  }, [])
   return (
     <>
-      <div className={styles.top}><Select
-        value={toOption(language1)}
-        onChange={(nv) => nv && setLanguage1(nv.value)}
-        options={toOptions(languages)}
-      />
-      <button><MaterialSymbol symbol="swap_horiz" className={styles.compose}
-        hoverable={false}/></button>
-      <Select
-        value={toOption(language2)}
-        onChange={(nv) => nv && setLanguage2(nv.value)}
-        options={toOptions(omit(languages, ['Autodetect']))}
-      />
-      <button onClick={() => navigate(paths.history)}><MaterialSymbol symbol="history" className={styles.compose}/></button>
+      <div className={styles.top}>
+        <Select
+          value={toOption(language1)}
+          onChange={(nv) => nv && setLanguage1(nv.value)}
+          options={toOptions(languages)}
+        />
+        <button><MaterialSymbol symbol="swap_horiz" className={styles.compose}
+          hoverable={false}/></button>
+        <Select
+          value={toOption(language2)}
+          onChange={(nv) => nv && setLanguage2(nv.value)}
+          options={toOptions(omit(languages, ['Autodetect']))}
+        />
+        <button onClick={() => navigate(paths.history)}><MaterialSymbol symbol="history" className={styles.compose}/></button>
       </div>
       <div>Press enter in the first field to translate</div>
       <div className={styles.bottom}>
         <textarea ref={textareaRef} onKeyPress={textareaOnKeypress}/>
-        <textarea readOnly value={result}/>
+        <textarea readOnly value={output}/>
       </div>
       
 
